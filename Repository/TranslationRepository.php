fd36a551336fde5b9ba6a773e4c249df1e8c799b
dev <dev@dev.com>
2026-03-03 12:13:49 +0300

    add PaginationEntityRepositoryTrait to TranslationRepository

--- Repository/TranslationRepository.php
+++ Repository/TranslationRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace ChamberOrchestra\TranslationBundle\Repository;
 
 use ChamberOrchestra\DoctrineExtensionsBundle\Repository\ServiceEntityRepository;
+use ChamberOrchestra\PaginationBundle\Repository\PaginationEntityRepositoryTrait;
 use ChamberOrchestra\TranslationBundle\Entity\Translation;
 use ChamberOrchestra\TranslationBundle\Utils\TranslationHelper;
 use Doctrine\Persistence\ManagerRegistry;
@@ -12,6 +13,8 @@ use Symfony\Component\Uid\Uuid;
 
 class TranslationRepository extends ServiceEntityRepository
 {
+    use PaginationEntityRepositoryTrait;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Translation::class);
